24e0bb3007c7a561bde4a6d3a1bc4007196a71b4
sinano1107 <tftf161107@icloud.com>
2023-05-24 12:30:39 +0900

    [update] UnityからブラウザJSにアクセス

---
+++ HTML+Unity/Assets/Plugins/sample.jslib
@@ -0,0 +1,35 @@
+mergeInto(LibraryManager.library, {
+  // 関数呼び出し
+  Hello: function () {
+    window.alert('Hello, world!')
+  },
+
+  // 数値型の引数と戻り値
+  AddNumbers: function (x, y) {
+    return x + y
+  },
+
+  // 数値型以外の型の引数
+  PrintFloatArray: function (array, size) {
+    for (var i = 0; i < size; i++) console.log(HEAPF32[(array >> 2) + i])
+  },
+
+  // 文字列型の引数
+  HelloString: function (str) {
+    window.alert(Pointer_stringify(str))
+  },
+
+  // 文字列の戻り値
+  StringReturnValueFunction: function () {
+    var returnStr = 'bla'
+    var bufferSize = lengthBytesUTF8(returnStr) + 1
+    var buffer = _malloc(bufferSize)
+    stringToUTF8(returnStr, buffer, bufferSize)
+    return buffer
+  },
+
+  // WebGLテクスチャのバインド
+  BindWebGLTexture: function (texture) {
+    GLctx.bindTexture(GLctx.TEXTURE_2D, GL.textures[texture])
+  },
+})
---
+++ HTML+Unity/Assets/Scripts/JsTest.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class JsTest : MonoBehaviour
+{
+
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    [DllImport("__Internal")]
+    private static extern void HelloString(string str);
+
+    [DllImport("__Internal")]
+    private static extern void PrintFloatArray(float[] array, int size);
+
+    [DllImport("__Internal")]
+    private static extern int AddNumbers(int x, int y);
+
+    [DllImport("__Internal")]
+    private static extern string StringReturnValueFunction();
+
+    [DllImport("__Internal")]
+    private static extern void BindWebGLTexture(int texture);
+
+    // スタート時に呼ばれる
+    void Start()
+    {
+        // 関数呼び出し
+        Hello();
+
+        // 数値型の引数と戻り値
+        int result = AddNumbers(5, 7);
+        Debug.Log(result);
+
+        // 数値型以外の型の引数
+        float[] myArray = new float[10];
+        PrintFloatArray(myArray, myArray.Length);
+
+        // 文字列型の引数
+        HelloString("This is a string.");
+
+        // 文字列の戻り値
+        Debug.Log(StringReturnValueFunction());
+
+        // WebGLテクスチャのバインド
+        var texture = new Texture2D(0, 0, TextureFormat.ARGB32, false);
+        BindWebGLTexture(texture.GetNativeTextureID());
+    }
+}
